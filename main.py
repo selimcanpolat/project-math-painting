fef7fe2a536e84d4c230ccc60f6330f696a38006
Selim Can Polat <selimcanpolat1@gmail.com>
2022-02-01 15:22:05 +0300

    Added CLI

--- main.py
+++ main.py
@@ -46,9 +46,45 @@ class Square:
     def draw(self, canvas):
         canvas.data[self.x : self.x + self.side, self.y : self.y+ self.side] = self.color
 
-my_canvas = Canvas(1000,1000,(255,255,255))
-my_rectangle = Rectangle(x=500, y=500, height=120, width=200, color=(0, 0, 200))
-my_rectangle.draw(my_canvas)
-my_square = Square(x=0, y=500, side=50, color=(250,0,0))
-my_square.draw(my_canvas)
-my_canvas.make("my_canvas.png")
+# my_rectangle = Rectangle(x=500, y=500, height=120, width=200, color=(0, 0, 200))
+# my_rectangle.draw(my_canvas)
+# my_square = Square(x=0, y=500, side=50, color=(250,0,0))
+# my_square.draw(my_canvas)
+# my_canvas.make("my_canvas.png")
+
+
+width = int(input("Please enter the width of the canvas: "))
+height = int(input("Please enter the height of the canvas: "))
+color = input("Please enter the color of the canvas in RGB format, eg. 0, 120, 255 : ")
+color = tuple(int(c) for c in color.split(", "))
+
+canvas_object = Canvas(width,height,color)
+shape = input("Square or rectangle? ")
+
+if shape == "Square" or  shape == "square":
+
+    square_x = int(input("Please enter the x of the square: "))
+    square_y = int(input("Please enter the y of the square: "))
+    side_square = int(input("Please enter the side length of the square: "))
+    color = input("Please enter the color of the square in RGB format, eg. 0, 120, 255 : ")
+    color = tuple(int(c) for c in color.split(", "))
+
+    square_object = Square(square_x, square_y, side_square, color)
+    square_object.draw(canvas_object)
+
+elif shape == "Rectangle" or shape == "rectangle":
+
+    rectangle_x = int(input("Please enter the x of the rectangle: "))
+    rectangle_y = int(input("Please enter the y of the rectangle: "))
+    width_rectangle = int(input("Please enter the width of the rectangle: "))
+    height_rectangle = int(input("Please enter the height of the rectangle: "))
+    color = input("Please enter the color of the rectangle in RGB format, eg. 0, 120, 255 : ")
+    color = tuple(int(c) for c in color.split(", "))
+
+    rectangle_object = Rectangle(rectangle_x, rectangle_y, height_rectangle, width_rectangle, color)
+    rectangle_object.draw(canvas_object)
+
+title = input(f"Please enter the title of your {width}x{height} canvas: ")
+canvas_object.make(title+".png")
+print("")
+print(f"{title}.png is ready.")
